0719a8434a6ab9600093262594c1f1e5d23905cb
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-29 17:15:21 +0000

    open the spreadsheet install libraries

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pandas as pd
+
+gsheetid = "1MjifIi5MPPGBP3635xWTrpef3RWngXcWgKjnCsaoE6Y"
+sheet_name ="Data"
+gsheet_url = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, sheet_name)
+df = pd.read_csv(gsheet_url)
+
+by_year = df.groupby('Year', sort=False)['Country'].count()
+#print('in these years have been producted\n', by_year)
+
+print('the movies with score 8 or higher')
+print(df[(df['IMDB Score'] > 8.0)])
+
