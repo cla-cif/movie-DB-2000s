cdb9e3b5e744a112fd8ce5de1dc26da0d0a8072d
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-03-30 07:28:50 +0000

    install seaborn library

--- run.py
+++ run.py
@@ -5,11 +5,14 @@ sheet_name ="Data"
 gsheet_url = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, sheet_name)
 df = pd.read_csv(gsheet_url)
 
+df_budget = df['Budget'].mean().round.astype(int)
+print('The average budget is', df_budget, '\n')
+
 by_year = df.groupby('Year', sort=False)['Country'].count()
 #print('in these years have been producted\n', by_year)
 
 sort_by_lowest = df.sort_values(by='IMDB Score', ascending=False)
-print(sort_by_lowest.head(10))
+#print(sort_by_lowest.head(10))
 
 sort_by_genere = df.loc[df.Genres == 'Comedy']
-print(sort_by_genere.head(10))
+#print(sort_by_genere.head(10))
