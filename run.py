1c660c2bfdadbbe8968d6d8b4b7067a80e44b47d
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-04-06 19:54:41 +0000

    edit descriptive text for consistency, movie >> film

--- run.py
+++ run.py
@@ -59,28 +59,28 @@ DATA AT A GLANCE
 def budget():
     df_budget = df['Budget'].dropna().mean().astype(int)
     print('\n'"The average cost to produce this decade's films is:", '{0:,}'.format(df_budget), '$' '\n')
-    print('Do you want to do a new search or find data?')
+    print('Do you want to do a new search or find data?\n')
     welcome()
 
 
 def score():
     df_score = df['IMDB Score'].mean().round(1)
     print('\n'"The average score got by this decade's films on IMDB is:", df_score, '\n')
-    print('Do you want to do a new search or find data?')
+    print('Do you want to do a new search or find data?\n')
     welcome()
 
 
 def language():
     df_language = df.groupby('Language')['Country'].size().sort_values(ascending=False).reset_index(name='Count')
     print('\n''Number of films in each language:\n', df_language.to_string(index=False), '\n')
-    print('Do you want to do a new search or find data?')
+    print('Do you want to do a new search or find data?\n')
     welcome()
 
 
 def year():
     df_year = df.groupby('Year', sort=False)['Country'].size().reset_index(name='Count')
     print('\n''Number of films produced each year:\n', df_year.to_string(index=False), '\n')
-    print('Do you want to do a new search or find data?')
+    print('Do you want to do a new search or find data?\n')
     welcome()
 
 """
@@ -98,14 +98,14 @@ def score_country():
 
 def highest_score():
     sort_highest_score = df[['Title', 'IMDB Score']].sort_values(by='IMDB Score', ascending=False)
-    print('\n''The best ten movies of the decade according to IMDB:\n', sort_highest_score.head(10).to_string(index=False), '\n')
+    print('\n''The best ten films of the decade according to IMDB:\n', sort_highest_score.head(10).to_string(index=False), '\n')
     print('Do you want to do a new search or find data?')
     welcome()
 
 
 def lowest_score():
     sort_lowest_score = df[['Title', 'IMDB Score']].sort_values(by='IMDB Score', ascending=True)
-    print('\n''The worst ten movies of the decade according to IMDB:\n', sort_lowest_score.head(10).to_string(index=False), '\n')
+    print('\n''The worst ten films of the decade according to IMDB:\n', sort_lowest_score.head(10).to_string(index=False), '\n')
     print('Do you want to do a new search or find data?')
     welcome()
 
@@ -114,7 +114,7 @@ def roi():
     df['ROI'] = (df["Gross Earnings"] / df["Budget"] * 100).round(2)
     sort_mostroi = df[['Title', 'ROI']].sort_values(
         by='ROI', ascending=False).dropna()
-    print('The most profitable movies of the decade:\n', sort_mostroi.head(10).to_string(index=False), '\n')
+    print('The most profitable films of the decade:\n', sort_mostroi.head(10).to_string(index=False), '\n')
     print('Do you want to do a new search or find data?')
     welcome()
 
@@ -142,8 +142,8 @@ def data_choice():
         3:  Number of films in each language.
         4:  Number of films produced each year.
         5:  Top ten countries that have produced films with the highest IMDB score.
-        6:  The best ten movies of the decade.
-        7:  The worst ten movies of the decade.
+        6:  The best ten films of the decade.
+        7:  The worst ten films of the decade.
         8:  The most profitable films in terms of return of investment.
         9:  Top 10 box-office flops: the most unprofitable films.
         10  The content ratings and their average IMDB Score.\n""")
@@ -195,28 +195,28 @@ SEARCH
 """
 
 
-def get_movie_info():
-    request_movie = input("Enter a Title: ")
-    request_movie = request_movie.lower().title()
+def get_film_info():
+    request_film = input("\nType a title: ")
+    request_film = request_film.lower().title()
     search = False
     for value in df.values:
         for item in value:
-            if request_movie in str(item):
+            if request_film in str(item):
                 search = True
 
     if search:
-        movie_data = df.loc[(df['Title'].str.contains(request_movie))]
-        print('All you need to know about the movie you were looking for\n', movie_data, '\n')
+        film_data = df.loc[(df['Title'].str.contains(request_film))]
+        print('All you need to know about the film you were looking for\n', film_data, '\n')
         print('Do you want to do a new search or find data?')
         welcome()
     else:
-        print('The movie is not present in the database')
+        print('The film is not present in the database')
         print('Do you want to do a new search or find data?')
         welcome()
 
 
-def get_movie_genres():
-    request_genre = input("Enter a genre: ")
+def get_film_genres():
+    request_genre = input('\nType a genre: ')
     request_genre = request_genre.lower().title()
     search = False
     for value in df.values:
@@ -226,8 +226,8 @@ def get_movie_genres():
 
     if search:
         mask = df['Genres'].str.contains(request_genre)
-        movie_genre = df.loc[mask, ['Title', 'Genres', 'Director', 'Cast']]
-        print('All the movies of the genre you were looking for\n', movie_genre, '\n')
+        film_genre = df.loc[mask, ['Title', 'Genres', 'Director', 'Cast']]
+        print('All the films of the genre you were looking for\n', film_genre, '\n')
         print('Do you want to do a new search or find data?')
         welcome()
     else:
@@ -242,7 +242,7 @@ def get_actor():
     df['Actor2'] = (df['Actor2'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
     df['Actor3'] = (df['Actor3'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
     """
-    request_actor = input("Enter an actor: ")
+    request_actor = input('\nType an actor: ')
     request_actor = request_actor.lower().title()
     search = False
     for value in df.values:
@@ -255,7 +255,7 @@ def get_actor():
         mask2 = df['Actor2'].str.contains(request_actor)
         mask3 = df['Actor3'].str.contains(request_actor)
         actor_data = df.loc[mask1 | mask2 | mask3, ['Title', 'Year', 'Genres', 'Actor1', 'Actor2', 'Actor3']]
-        print('All the movies of the actor you were looking for\n', actor_data, '\n')
+        print('All the films of the actor you were looking for\n', actor_data, '\n')
         print('Do you want to do a new search or find data?')
         welcome()
 
@@ -266,7 +266,7 @@ def get_actor():
 
 
 def get_director():
-    request_director = input("Enter a director: ")
+    request_director = input('\nType a director: ')
     request_director = request_director.lower().title()
     search = False
     for value in df.values:
@@ -277,13 +277,13 @@ def get_director():
     if search:
         mask = df['Director'].str.contains(request_director)
         director_data = df.loc[mask, ['Title', 'Genres', 'Director', 'Cast']]
-        print('All the movies from the director you were looking for\n', director_data, '\n')
+        print('All the films from the director you were looking for\n', director_data, '\n')
         print('Do you want to do a new search or find data?')
         welcome()
 
     else:
         print('The director is not present in the database\n')
-        print('Do you want to do a new search or find data?\n')
+        print('Do you want to do a new search or find data?')
         welcome()
 
 
@@ -302,9 +302,9 @@ def search_choice():
             elif user_input == 'actor':
                 get_actor()
             elif user_input == 'genre':
-                get_movie_genres()
+                get_film_genres()
             elif user_input == 'title':
-                get_movie_info()
+                get_film_info()
             break
     return user_input
 
@@ -330,13 +330,14 @@ title = pyfiglet.figlet_format("Movie DB 2000s", font = "slant" )
 print(Fore.YELLOW + Style.BRIGHT + title)
 print(Fore.CYAN + Style.BRIGHT + """
 Welcome to the 2000s Movie Database, 
-the database contains""", df['Title'].count(), """films cathegorised by 
-title, genre, year, director, the three main actors, 
-number of reviews (from critics and users) and IMDB score.\n""" +
-Fore.YELLOW + Style.BRIGHT + """
-Get statistics, the top 10 lists or search by film.
-""" + Style.RESET_ALL + """
-What do you want to do today, get data or search?\n""" + Style.BRIGHT +
-"""(To quit, type exit after a question)\n""")
+the database contains""", df['Title'].count(), """films cathegorised by
+title, genre, year, director, cast,
+number of reviews (from critics and users) and IMDB score.\n""" 
++ Fore.YELLOW + Style.BRIGHT + """
+Get statistics, the top 10 lists or search by film.\n""" 
++  Style.RESET_ALL + """
+To quit, type exit after a question | To run, hit enter\n""" 
++ Style.BRIGHT + """
+What do you want to do today, get data or search?\n""")
 
 main()
