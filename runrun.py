67dfa38f41edbc8f2ed461b909c39af36dfb3ab5
Claudia Cifaldi <claudia.cifaldi@gmail.com>
2022-04-16 16:16:47 +0000

    remove old screenshot and replace with new ones, function genres throws error float is not iterable, add to runrun.py to fix

--- runrun.py
+++ runrun.py
@@ -11,47 +11,47 @@ GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{GSHEET_ID}/gviz/tq?tqx=ou
 df = pd.read_csv(GSHEET_URL)
 
 
-def get_film_info():
+def get_film_genres():
     """
     applies to input .title() to match dataframe's entries case
-    search for matches in the dfcopy of Title column,
-    if none found, encodes entries to match search for extend ASCII chars,
-    if none found, display message. else display output.
+    search for matches in the Genres column,
+    display output or message, calls welcome function after.
+    if more than 10 matches found, displays a random .sample() of them.
     """
     print(
         Fore.WHITE + Style.BRIGHT +
-        """\n    Search by full or partial title,
+        """\n    Search by one or more genres,
     type the words divided by a space.
-    Characters from a foreign alpabet will be matched as well
-    (type amelie to match Amélie)
-    In case of multiple matches,
-    the results will be restricted to the first 10 elements""")
-    request_film = input(f"{Fore.BLUE + Style.BRIGHT}"
-                         f"\nType a title: {Style.RESET_ALL}")
-    request_film = request_film.lower().title().strip().str
-    print('this is the input', request_film)
+    In case of multiple matches ('Comedy' has 850+ !),
+    only 10 random results will be displayed.""")
+    request_genre = input(f"{Fore.BLUE + Style.BRIGHT}"
+                          f"\nType a genre: {Style.RESET_ALL}")
+    request_genre = request_genre.lower().title().strip()
     search = False
     df_copy = df.copy(deep=True)
-    print(df_copy['Title'].str.lower)
-    for value in df_copy['Title']:
-        if request_film in value:
+    df_copy.dropna().astype(str)
+    for value in df_copy['Genres']:
+        if request_genre in value:
             search = True
-    if not search:
-        df_copy['Title'] = (
-            df_copy['Title'].str.normalize('NFKD').str.encode(
-                'ascii', errors='ignore').str.decode('utf-8'))
-        for value in df_copy['Title']:
-            if request_film in value:
-                search = True
-
     if search:
-        film_data = df.loc[(df_copy['Title'].str.contains(request_film))]
+        mask = df_copy['Genres'].str.contains(request_genre)
+        film_genre = df_copy.loc[mask,
+                                 ['Title',
+                                  'Genres',
+                                  'Director',
+                                  'Actor1',
+                                  'Actor2',
+                                  'Actor3',
+                                  'IMDB Score']]
+        if mask.sum() > 10:
+            film_genre = film_genre.sample(n=10, replace=True)
         print(f"""{Fore.YELLOW + Style.BRIGHT}
-All you need to know about the film you were looking for:
-{Style.RESET_ALL}{film_data}\n""")
+All the films of the genre you were looking for:
+{Style.RESET_ALL}{film_genre}\n""")
 
     else:
         print(f"""{Fore.YELLOW + Style.BRIGHT}
-This film is not present in the database{Style.RESET_ALL}\n""")
-get_film_info()
+This genre is not present in the database{Style.RESET_ALL}\n""")
 
+
+get_film_genres()
